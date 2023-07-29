f0e8555f3f16a5de95254f733b65132d050c5d0c
gaya3sooraj <33348580+gaya3sooraj@users.noreply.github.com>
2023-07-29 23:22:16 +0100

    Update tts.py

--- tts.py
+++ tts.py
@@ -13,10 +13,6 @@ def index():
         text = request.form['text']
         language = request.form['language']
         return render_template('index.html', result=text, s=language)
-        #tts = gTTS(text=text, lang="en", tld=language)
-        #audio_file = 'static/audio_output.mp3'
-        #tts.save(audio_file)
-        #return audio_file
     else:
         return render_template('index.html')
 
